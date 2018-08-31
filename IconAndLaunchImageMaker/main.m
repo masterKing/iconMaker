da3661a1a9fd839ad25c198cb7a651b0ceb4bcdd
Franky's iMac <548913086@qq.com>
2018-08-31 17:56:02 +0800

    换一个名字,添加了新的代码

--- IconAndLaunchImageMaker/main.m
+++ IconAndLaunchImageMaker/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  IconAndLaunchImageMaker
+//  iconAndLaunchImageMaker
 //
 //  Created by Franky on 2018/8/31.
 //  Copyright © 2018年 Franky. All rights reserved.
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # iOSIconAndLaunchImageMaker
 一款Mac终端命令行工具,用来自动生成iOS应用图标,后期会添加生成启动图片功能
+`
+`
